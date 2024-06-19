6f88e5a409dfb3b0be21d8d92915986a6d6771b5
Helloworld2523 <helloworld2523@gmail.com>
2024-06-19 12:48:45 +0700

    update sch.dart in folder store

---
+++ lib/store/sch.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:th.ac.ru.uSmart/model/rotcs_register.dart';
+import 'package:th.ac.ru.uSmart/model/scholarship.dart';
+
+class SchStorage {
+  static const String key = 'sch';
+  static Future<void> saveSch(Scholarship sch) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final schJson = sch.toJson();
+    await prefs.setString(key, jsonEncode(schJson));
+  }
+
+  static Future<Scholarship> getSch() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final schString = prefs.getString(key);
+    print('cache storage: $schString');
+    if (schString != null) {
+      final schJson = jsonDecode(schString);
+      return Scholarship.fromJson(schJson);
+    }
+
+    return Scholarship();
+  }
+
+  static Future<void> removeSch() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    print('clear cache....');
+    await prefs.setString(key, '{}');
+  }
+}
